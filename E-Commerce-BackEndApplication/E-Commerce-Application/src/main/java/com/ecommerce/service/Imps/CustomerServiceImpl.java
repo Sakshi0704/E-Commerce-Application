95f15437d5d90f1b79228d334ea924799bbab595
Sakshi0704 <banichoudhary0704@gmail.com>
2023-11-01 21:25:15 +0530

    updated the controller

--- E-Commerce-BackEndApplication/E-Commerce-Application/src/main/java/com/ecommerce/service/Imps/CustomerServiceImpl.java
+++ E-Commerce-BackEndApplication/E-Commerce-Application/src/main/java/com/ecommerce/service/Imps/CustomerServiceImpl.java
@@ -1,6 +1,8 @@
 
 package com.ecommerce.service.Imps;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import com.ecommerce.exception.DataAlreadyExitsException;
 
 import com.ecommerce.exception.ResourceNotFoundException;
 import com.ecommerce.model.Customer;
+import com.ecommerce.model.Image;
 import com.ecommerce.repository.CustomerRepository;
 import com.ecommerce.service.CustomerService;
 
@@ -49,14 +52,68 @@ public class CustomerServiceImpl implements CustomerService{
 
 
 	@Override
-	public Customer updateCustomerDetails(String contact, Customer customer) throws ResourceNotFoundException {
+	public Customer updateCustomerDetails(String email, Customer customer) throws ResourceNotFoundException {
+		// TODO Auto-generated method stub
+		Optional<Customer> exitCustomer = customerRepo.findByEmail(email);
+		
+		
+		return null;
+	}
+
+
+	@Override
+	public Customer searchByContact(String contact) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
-	
-	
-	
+
+
+	@Override
+	public Customer deleteCustomerAccount(String email) throws ResourceNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> searchByfirstName(String firstName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> getAllCustomerDetails(Integer page, Integer size, String sortDirection, String sortBy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> searchBylastName(String lastName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> searchByFirstAndLastName(String firstName, String lastName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Customer updateCustomerImage(String email, Image image) throws ResourceNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public String deleteCustomerImage(String fileName) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
